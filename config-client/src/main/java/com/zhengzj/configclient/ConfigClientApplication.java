eeeb297fe4392bf56b6a9c7dc832365b7a7c5f92
zhengzj <1261383096@qq.com>
2019-01-18 18:36:06 +0800

    create eureka-server client,config-server client

---
+++ config-client/src/main/java/com/zhengzj/configclient/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.zhengzj.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RefreshScope
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${hello.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return hello;
+    }
+
+}
+
---
+++ eureka-server/src/main/java/com/zhengzj/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.zhengzj.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+@EnableConfigServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
+
